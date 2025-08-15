bc473692710171aba77da6908a55ea3664422491
Sthefany Albuquerque <s.albuquerque@aluno.ifsp.edu.br>
2025-08-15 11:37:23 -0300

    Secao 4 - Aprimorando os testes: aula 1

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -20,4 +20,32 @@ describe('tarefas', ()=> {
   
   })
 
+  it('não deve permitir tarefa duplicada', () => {
+    
+    cy.request({
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'DELETE',
+      body: {name: 'Estudar JavaScript'}
+    }).then(response => {
+        expect(response.status).to.eq(204)
+    })
+
+    cy.request({
+      url: 'http://localhost:3333/tasks',
+      method: 'POST',
+      body: {name: 'Estudar JavaScript', is_done: false}
+    }).then(response => {
+        expect(response.status).to.eq(201)
+    })
+
+    cy.visit('http://localhost:3000')
+
+    cy.get('input[placeholder="Add a new Task"]').type('Estudar JavaScript')
+
+    cy.contains('button', 'Create').click()
+    
+    cy.get('.swal2-html-container').should('be.visible').should('have.text', 'Task already exists!')
+
+  });
+
 })
