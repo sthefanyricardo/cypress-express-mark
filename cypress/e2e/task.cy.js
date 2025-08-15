ccb4db5f35aabc368b3959b7b9499b755f52b550
Sthefany Albuquerque <s.albuquerque@aluno.ifsp.edu.br>
2025-08-15 11:14:58 -0300

    Segunda suite de teste desenvolvida, com cenarias que testam a aplicacao api e web

---
+++ cypress/e2e/task.cy.js
@@ -0,0 +1,23 @@
+describe('tarefas', ()=> {
+
+  it('deve cadastrar uma nova tarefa', () => {
+
+    cy.request({
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'DELETE',
+      body: {name: 'Ler um livro de node.js'}
+    }).then(response => {
+        expect(response.status).to.eq(204)
+    })
+
+    cy.visit('http://localhost:3000')
+
+    cy.get('input[placeholder="Add a new Task"]').type('Ler um livro de node.js')
+
+    cy.contains('button', 'Create').click()
+    
+    cy.contains('main div p', 'Ler um livro de node.js').should('be.visible')
+  
+  })
+
+})
